14201c900821df8f07071db6f9ad827938398607
zootya <levelkardoszoltannak@gmail.com>
2024-12-14 12:44:01 +0100

    models

--- backend/turisztikailatvanyossagok/admin.py
+++ backend/turisztikailatvanyossagok/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Orszag, Varos, Latvanyossag
 # Register your models here.
+
+admin.site.register(Orszag)
+admin.site.register(Varos)
+admin.site.register(Latvanyossag)
--- backend/turisztikailatvanyossagok/models.py
+++ backend/turisztikailatvanyossagok/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+
+class Orszag(models.Model):
+    orszagMegnevezes = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.orszagMegnevezes
+    class Meta:
+        verbose_name_plural="Országok"
+
+    
+class Varos(models.Model):
+    varosMegnevezese = models.CharField(max_length=100)
+    orszag = models.ForeignKey("Orszag", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.varosMegnevezese
+    class Meta:
+        verbose_name_plural="Városok"
+    
+class Latvanyossag(models.Model):
+    latvanyossagMegnevezes = models.CharField(max_length=100)
+    latvanyossagLeiras = models.TextField()
+    ertekeles = models.IntegerField(
+        default = 0 ,
+        validators = [
+            MinValueValidator(0),
+            MaxValueValidator(5)
+        ]
+    )
+    nyitvatartas = models.CharField(max_length=100)
+    kepUrl = models.CharField(max_length=100)
+    varos = models.ForeignKey("Varos", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.latvanyossagMegnevezes
+    class Meta:
+        verbose_name_plural="Látványosságok"
