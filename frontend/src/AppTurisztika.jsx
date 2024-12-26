93c1ae27ae36d892190a79e579c9cbffae4c8110
zootya <levelkardoszoltannak@gmail.com>
2024-12-27 00:03:05 +0100

    vite listazas

--- frontend/src/AppTurisztika.jsx
+++ frontend/src/AppTurisztika.jsx
@@ -3,14 +3,31 @@ import MyHeaderMenu from './MyHeaderMenu.jsx'
 import MyFooterMenu from './MyFooterMenu.jsx'
 import Attrakcio from './Attrakcio.jsx'
 
+import { useEffect, useState } from 'react';
+
 function AppTurisztika() {
+    const myUrl = "http://127.0.0.1:8000";
+    const [data, setdata] = useState([]);
+
+    useEffect(() => {
+        fetch(myUrl+"/attrakcio")
+        .then((response) => {response.json()
+            .then((res) => {
+                setdata(res)
+            } )
+        })
+    },[]);
+
+    console.log(data);
+
     return (
         <div>
-            <header>
+            <div>
                 <MyHeaderMenu />
-            </header>
+            </div>
 
-            <container>
+            <div>
+            {/*                
                 <Attrakcio 
                     kepUrl = "https://i.szalas.hu/pois/274/original/16300.jpg" 
                     latvanyossagMegnevezes = "Református Nagytemplom"
@@ -19,22 +36,26 @@ function AppTurisztika() {
                     varos = "Debrecen - Magyarország"
                     ertekelesLink = "https://i.szalas.hu/pois/274/original/16300.jpg"
                 />
+            */}
 
+                {data.map( (item, key) =>
 
-                <Attrakcio 
-                    kepUrl = "https://i.szalas.hu/pois/274/original/16300.jpg" 
-                    latvanyossagMegnevezes = "Református Nagytemplom"
-                    nyitvatartas = "H: zárva; K: 9-17; Sze: 9-17; Cs: 9-19; P: 9-17; Szo: 9-17; V: 8-20"
-                    latvanyossagLeiras = "A debreceni református nagytemplom Debrecen legjellegzetesebb épülete, 1500 m²-es alapterületével az ország legnagyobb református temploma, ezenkívül a legnagyobb protestáns templom is az országban. A Magyarországi Református Egyház jelképe, miatta nevezik Debrecent gyakran „kálvinista Rómának”. 1805 és 1824 között épült, klasszicista stílusban, a Kossuth tér és a Kálvin tér közt áll, a belvárosban."
-                    varos = "Debrecen - Magyarország"
-                    ertekelesLink = "https://i.szalas.hu/pois/274/original/16300.jpg"
-                />
-
-            </container>
+                    <Attrakcio 
+                        id = {item.id}
+                        kepUrl = {item.kepUrl}
+                        latvanyossagMegnevezes = {item.latvanyossagMegnevezes}
+                        nyitvatartas = {item.nyitvatartas}
+                        latvanyossagLeiras = {item.latvanyossagLeiras}
+                        varos = {item.varos.varosMegnevezes} 
+                        orszag = {item.varos.orszag.orszagMegnevezes}
+                        ertekelesLink = {item.id}
+                    />
+                )}
+            </div>
             
-            <footer>
+            <div>
                 <MyFooterMenu />
-            </footer>
+            </div>
         </div>
     )
 }
--- frontend/src/Attrakcio.jsx
+++ frontend/src/Attrakcio.jsx
@@ -8,13 +8,13 @@ function Attrakcio(props) {
                     <div class="d-flex position-relative m-2">
                         <img class="card-img-right m-2" src={props.kepUrl} className="kep" alt="kep" height = "200vh" />
                         <div class = "info bg-body-secondary p-2 m-2">    
-                            <h2 class="card-title">{props.latvanyossagMegnevezes}</h2>
+                            <h2 class="card-title">{props.id} - {props.latvanyossagMegnevezes}</h2>
                             <p class = "cars-text">{props.latvanyossagLeiras}</p>
                             <p class = "cars-text">Nyítvatartás: {props.nyitvatartas}</p>
-                            <h6 class = "cars-title">{props.varos}</h6>
+                            <h6 class = "cars-title">{props.varos} - {props.orszag}</h6>
                         </div>
                         <div>
-                            <a href={props.ertekelesLink} class="btn btn-primary stretched-link m-2">Értékelés</a>
+                            <a href={"http://127.0.0.1:3000/ertekeles.html?id=" + props.ertekelesLink} class="btn btn-primary stretched-link m-2">Értékelés</a>
                         </div>
                     </div>
                 </div>
